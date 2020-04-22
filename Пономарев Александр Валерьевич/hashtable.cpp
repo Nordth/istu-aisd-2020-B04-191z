c5a496a129cc02200d11e36501b2b454cecf58f6
Amindlog <Amind@bk.ru>
2020-04-22 16:34:03 +0400

    1

--- Пономарев Александр Валерьевич/hashtable.cpp
+++ Пономарев Александр Валерьевич/hashtable.cpp
@@ -1,1 +1,26 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <stdio.h>
+
+using  namespace std;
+
+int hh(string str); //hash
+
+struct listHash
+{
+    int value;
+    string word;
+    listHash *next;
+};
+struct tablehash {
+	listHash* array;
+	int size = 0; //количество записей в списке
+	int elements = 0; //сколько всего элементов в списке
+};
+int main() {
+    
+    return 0;
+}
+
+ 

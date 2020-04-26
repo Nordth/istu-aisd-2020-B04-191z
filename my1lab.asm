a49068fc713f6bdd894adbbe1e1ecabb15aca652
Amindlog <Amind@bk.ru>
2020-04-26 18:30:30 +0400

    асм + граф

--- my1lab.asm
+++ my1lab.asm
@@ -1,34 +1,60 @@
-.386
-.model  flat,stdcall
-option  casemap:none
- 
-    include \masm32\include\windows.inc
-    include \masm32\include\masm32.inc
-    include \masm32\include\kernel32.inc
-    include \masm32\include\user32.inc
-    include \masm32\macros\macros.asm
-    includelib \masm32\lib\masm32.lib
-    includelib \masm32\lib\kernel32.lib
-    includelib \masm32\lib\user32.lib
-
+.486 ;процессор для выполнения
+.model flat, stdcall 
 .data
-msg1        db  "Результат",0 ;Для вывода в окне
-temp dw ?
-
-
-a dd 11d
-b dd 5d
-rslt dd ?
-
-.code
-start:
-mov eax, [a]
-mov ebx, [b]
-add eax, ebx
-add eax, 30h
-;invoke atodw, eax
-mov [rslt], eax
-
-invoke StdOut, addr rslt
-invoke ExitProcess, 0
+m   db  "a1s3108g", 0 ;;Даннные как строка
+c1  dw  0 ;;Слово
+ 
+.code ;;парвила
+start proc near ;;обьявление процедуры
+    lea esi, m ;;отдаем в esi данные
+    xor bx, bx ;;смещение в массиве на 1
+;;цикл 1   
+m1: mov al, byte ptr [esi]
+    ; сравнение 
+    cmp al, "1"
+    ; если не равно переходим на М3
+    jne m3
+    ; увеличение на 1 или смешение на 1
+    inc bx
+;;цикл 2   
+m3: mov al, byte ptr [esi] ;указываем на адрес памяти по элементу
+; сравниваем
+    cmp al, "3"
+    ; если не равно то переходим на М2
+    jne m2
+    ; смещение на 1
+    inc bx
+; цикл 3
+m2: inc esi ;если равно 0
+    cmp al,0
+    jne m1
+    mov c1,bx 
+    lea esi,m
+    mov edi,esi
+; цикл 4
+m6: lodsb
+    test al,al
+    jz ex
+    cmp al,'0'
+    jc m4
+    cmp al,'9'
+    ja m4
+    jmp m6
+; цилк 5
+m4: stosb
+    jmp m6
+    ; выполнить
+ex: mov esi,edi
+; цикл
+m5: lodsb
+    test al,al
+    jz ex1
+    mov al,0
+    stosb
+    jmp m5
+; выполнить
+ex1:ret
+; код закончен
+start endp
+; программа завершина
 end start

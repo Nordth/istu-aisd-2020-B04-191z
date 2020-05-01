138fee50dcf7c64c044b8c10c20f3af2a8057965
MaxYakimov <64300616+MaxYakimov@users.noreply.github.com>
2020-05-01 08:59:41 +0500

    Лабораторная №2 Якимов М.В.
    Переделал отдельно слияние в процедуре, она вызывается после рекурсивного разбиения массива на подмассивы.

--- Якимов Максим Вячеславович/Лабораторная работа №2/Source_sliyanie.cpp
+++ Якимов Максим Вячеславович/Лабораторная работа №2/Source_sliyanie.cpp
@@ -7,7 +7,40 @@ int random(int N) {
 	return (rand() % N) + 1;
 }
 
-int* merge(int *a, int begin, int end, int *b) {
+int* proc_merge(int *a, int *b, int left, int begin, int end) {
+	int right = end + 1;
+	int ai = begin;
+	int aj = left;
+	int bi = ai;
+	
+	while (ai < left&&aj < right) {
+		if (a[ai] < a[aj]) {
+			b[bi] = a[ai];
+			ai++;
+		}
+		else {
+			b[bi] = a[aj];
+			aj++;
+		}
+		bi++;
+	}
+	while (ai < left) {
+		b[bi] = a[ai];
+		bi++;
+		ai++;
+	}
+	while (aj < right) {
+		b[bi] = a[aj];
+		bi++;
+		aj++;
+	}
+	for (int i = begin; i < end + 1; i++) {
+		a[i] = b[i];
+	}
+	return a;
+}
+
+int* recurs_div(int *a, int begin, int end, int *b) {
 	int buff;
 	int size = end - begin + 1;
 	if (size == 1) {
@@ -23,39 +56,10 @@ int* merge(int *a, int begin, int end, int *b) {
 	}
 	else {
 		int half = (end - begin) / 2 + begin;
-		a = merge(a, begin, half, b);
-		a = merge(a, half + 1, end, b);
+		a = recurs_div(a, begin, half, b);
+		a = recurs_div(a, half + 1, end, b);
+		a = proc_merge(a, b, half+1, begin, end);
 		
-		int left = half + 1;
-		int right = end + 1;
-		int ai = begin;
-		int aj = half + 1;
-		int bi = begin;
-
-		while (ai < left&&aj < right) {
-			if (a[ai] < a[aj]) {
-				b[bi] = a[ai];
-				ai++;
-			}
-			else {
-				b[bi] = a[aj];
-				aj++;
-			}
-			bi++;
-		}
-		while (ai < left) {
-			b[bi] = a[ai];
-			bi++;
-			ai++;
-		}
-		while (aj < right) {
-			b[bi] = a[aj];
-			bi++;
-			aj++;
-		}
-		for (int i = begin; i < end + 1; i++) {
-			a[i] = b[i];
-		}
 		return a;
 	}
 }
@@ -74,7 +78,7 @@ int main() {
 		cout << A[i]<<' ';
 	}
 	cout << '\n';
-	A = merge(A, 0, N-1, B);
+	A = recurs_div(A, 0, N-1, B);
 
 	for (int i = 0; i < N; i++) {
 		cout << A[i] << ' ';

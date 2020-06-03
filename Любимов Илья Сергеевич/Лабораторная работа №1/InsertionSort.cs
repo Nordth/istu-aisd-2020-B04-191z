da24073ab140c28a44b552d9eff59790cc29da93
naitum-ux <59657416+naitum-ux@users.noreply.github.com>
2020-06-03 14:06:19 +0300

    Исправил

--- Любимов Илья Сергеевич/Лабораторная работа №1/InsertionSort.cs
+++ Любимов Илья Сергеевич/Лабораторная работа №1/InsertionSort.cs
@@ -6,29 +6,30 @@ namespace InsertionSort
         static void Main(string[] args)
         {
             Console.Write("Введите элементы массива через пробелы: ");
-            string read_arr = Console.ReadLine().Trim(' ');
-            string[] arr = read_arr.Split(' ');
+            string read_arr = Console.ReadLine();
+            string[] string_arr = read_arr.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] arr = Array.ConvertAll(string_arr, int.Parse);
+            Console.WriteLine("Массив до сортировки: {0}", string.Join(", ", arr));
             arr = InsertionSort(arr);
-
-            Console.Write("Результат сортировки вставками: {0}", string.Join(", ",arr));
+            Console.Write("Результат сортировки вставками: {0}", string.Join(", ", arr));
             Console.ReadLine();
         }
-        public static string[] InsertionSort(string[] arr)
+        public static int[] InsertionSort(int[] arr)
         {
-            int lenght = arr.Length;
-            string byf;
-            for (var i = 0; i < lenght; i++)
+            int length = arr.Length;
+            int byf;
+            for (var i = 0; i < length; i++)
             {
-                int key = int.Parse(arr[i]);
+                int key = arr[i];
                 int j = i;
-                while ((j > 0) && (int.Parse(arr[j - 1]) > key))
+                while ((j > 0) && (arr[j - 1] > key))
                 {
                     byf = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = byf;
                     j--;
                 }
-                arr[j] = key.ToString();
+                arr[j] = key;
             }
             return arr;
         }
--- Любимов Илья Сергеевич/Лабораторная работа №1/QuickSorting.cs
+++ Любимов Илья Сергеевич/Лабораторная работа №1/QuickSorting.cs
@@ -6,23 +6,22 @@ namespace QuickSorting
         static void Main(string[] args)
         {
             Console.Write("Введите элементы массива через пробелы: ");
-            string read_arr = Console.ReadLine().Trim(' ');
-            string[] arr = read_arr.Split(' ');
-            // string[] arr = new string[7] { "4", "1", "6", "2", "1", "3", "5" };
+            string read_arr = Console.ReadLine();
+            string[] string_arr = read_arr.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] arr = Array.ConvertAll(string_arr, int.Parse);
             Console.WriteLine("Входной массив: {0}", string.Join(", ", arr));
-            QuickSorting(arr, 0, arr.Length-1);
+            QuickSorting(arr, 0, arr.Length - 1);
             Console.WriteLine("Результат быстрой сортирвки: {0}", string.Join(", ", arr));
             Console.ReadLine();
         }
-        public static void QuickSorting(string[] arr, int first, int last)
+        public static void QuickSorting(int[] arr, int first, int last)
         {
             int key = (last - first) / 2 + first, key_left = first, key_right = key + 1;
-            int i = first, j = last;
-            string buf;
-            while (i<=j)
+            int i = first, j = last, buf;
+            while (i <= j)
             {
-                while (int.Parse(arr[i]) < int.Parse(arr[key]) && i <= last) ++i;
-                while (int.Parse(arr[j]) > int.Parse(arr[key]) && j >= first) --j;
+                while (arr[i] < arr[key] && i <= last) ++i;
+                while (arr[j] > arr[key] && j >= first) --j;
                 if (i <= j)
                 {
                     buf = arr[i];
@@ -32,7 +31,7 @@ namespace QuickSorting
                 }
             }
             if (j > first) QuickSorting(arr, first, j);
-            if (i < last) QuickSorting(arr, i, last);    
+            if (i < last) QuickSorting(arr, i, last);
         }
     }
 }

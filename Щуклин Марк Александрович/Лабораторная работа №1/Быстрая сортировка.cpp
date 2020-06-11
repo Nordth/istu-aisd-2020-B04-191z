837a2a7ae35b613cc6337bdf8789919298a3d2cb
Щуклин Марк <mrks@ro.ru>
2020-06-11 16:46:59 +0400

    Исходная версия 1 лаб + 2 лаб

---
+++ Щуклин Марк Александрович/Лабораторная работа №1/Быстрая сортировка.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+long long int arr[100005];
+
+int partition(int l, int r)
+{
+	int v = arr[(l + r) / 2];
+	int i = l;
+	int j = r;
+	while (i <= j)
+	{
+		while (arr[i] < v)
+		{
+			i = i + 1;
+		}
+		while (arr[j] > v)
+		{
+			j = j - 1;
+		}
+		if (i >= j)
+		{
+			break;
+		}
+		std::swap(arr[i++], arr[j--]);
+	}
+	return j;
+}
+
+void QuickSort(int begin, int end)
+{
+	if (begin >= end)
+	{
+		return;
+	}
+	int pos = partition(begin, end);
+	QuickSort(begin, pos);
+	QuickSort(pos + 1, end);
+}
+
+
+int main()
+{
+	std::cout << "Number of elements:" << std::endl;
+	int n;
+	std::cin >> n;
+	std::cout << "Elements:" << std::endl;
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> arr[i];
+	}
+	QuickSort(0, n - 1);
+	std::cout << "Result:" << std::endl;
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << arr[i] << ' ';
+	}
+	//system("PAUSE");
+	return 0;
+}

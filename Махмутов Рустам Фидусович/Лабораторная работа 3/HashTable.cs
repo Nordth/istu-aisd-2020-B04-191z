b3334aeab4645005c14dff617fb430153a6e866f
Rustam Mahmutov <54999592+molodoichelovekproidemte@users.noreply.github.com>
2021-03-31 22:12:59 +0500

    lab 3

---
+++ Махмутов Рустам Фидусович/Лабораторная работа 3/HashTable.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+
+    class HashTable
+    {
+        private int tableSize;
+        private chainItem[] table;
+        class chainItem
+        {
+            public int Key;
+            public string Value;
+            public chainItem Next = null;
+
+        }
+        public HashTable(int tableSize)
+        {
+            this.tableSize = tableSize;
+            table = new chainItem[this.tableSize];
+
+        }
+
+        private int hash(int key)
+        {
+            return key % tableSize;
+        }
+
+        public bool addValue(int key, string val)
+        {
+            int hv = hash(key);
+            var item = this.table[hv];
+
+            if (item == null)
+            {
+                this.table[hv] = new chainItem();
+                this.table[hv].Key = key;
+                this.table[hv].Value = val;
+                return true;
+            }
+            chainItem prev = null;
+            while (item != null)
+            {
+                if (item.Key == key)
+                    break;
+                prev = item;
+                item = item.Next;
+            }
+
+            if (item != null)
+            {
+                return false;
+            }
+            else
+            {
+
+                var tmp = new chainItem();
+                prev.Next = tmp;
+                tmp.Key = key;
+                tmp.Value = val;
+            }
+            return true;
+
+        }
+
+
+        public bool setValue(int key, string val)
+        {
+
+            int hv = hash(key);
+
+            var item = this.table[hv];
+
+            if (item == null)
+            {
+                return false;
+            }
+
+
+            while (item != null)
+            {
+                if (item.Key == key)
+                    break;
+                item = item.Next;
+            }
+
+            if (item == null)
+            {
+                return false;
+            }
+            else
+            {
+                item.Value = val;
+            }
+            return true;
+
+        }
+
+
+
+        public bool cleanValue(int key)
+        {
+
+            int hv = hash(key);
+  
+            var item = this.table[hv];
+
+
+            if (item == null)
+            {
+                return false;
+            }
+
+
+            chainItem prev = null;
+            while (item != null)
+            { 
+               
+                if (item.Key == key)
+                    break;
+                prev = item;
+                item = item.Next;
+            }
+
+            if (item == null)
+                return false;
+            else
+            {
+
+                if (prev == null)
+                {
+                    this.table[hv] = item.Next;
+                }
+                else
+                {
+                    prev.Next = item.Next;
+                }
+            }
+            return true;
+
+        }
+
+       
+        public bool getValue(int key, ref string y)
+        {
+            
+            var item = this.table[hash(key)];
+            
+            while (item != null)
+            {
+                if (item.Key == key)
+                {
+                    y = item.Value;
+                    return true;
+                }
+                item = item.Next;
+            }
+            
+            return false;
+        }
+    }
+}
---
+++ Махмутов Рустам Фидусович/Лабораторная работа 3/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable tbl = new HashTable(100);
+            string ch = "";
+            while (ch != "q")
+            {
+                Console.Clear();
+                Console.WriteLine("[1] - Добавление");
+                Console.WriteLine("[2] - Запись");
+                Console.WriteLine("[3] - Поиск");
+                Console.WriteLine("[4] - Удаление");
+                Console.WriteLine("[q] - Выход");
+                ch = Console.ReadLine();
+                int x = 0;
+                bool r = false;
+                int ind = 0;
+                string y = "";
+                try
+                {
+                    switch (ch)
+                    {
+                        case "1":
+
+                            Console.WriteLine("Введите индекс");
+                            x = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("Введите значение");
+                            y = Console.ReadLine();
+                            if (tbl.addValue(x, y))
+                                Console.WriteLine("Элемент добавлен");
+                            else
+                                Console.WriteLine("Элемент с указанным ключом уже существует");
+
+                            break;
+                        case "2":
+
+                            Console.WriteLine("Введите индекс");
+                            x = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("Введите значение");
+                            y = Console.ReadLine();
+                            if (tbl.setValue(x, y))
+                                Console.WriteLine("Элемент изменен");
+                            else
+                                Console.WriteLine("Элемент с указанным ключом не существует");
+
+
+                            break;
+                        case "3":
+
+                            Console.WriteLine("Введите индекс");
+                            x = Int32.Parse(Console.ReadLine());
+                            if (tbl.getValue(x, ref y))
+                                Console.WriteLine("Значение  = {0}", y);
+                            else
+                                Console.WriteLine("Элемент не найден");
+                            break;
+                        case "4":
+                            Console.WriteLine("Введите индекс");
+                            ind = Int32.Parse(Console.ReadLine());
+                            if (tbl.cleanValue(ind))
+                                Console.WriteLine("Элемент удален");
+                            else
+                                Console.WriteLine("Элемент с указанным ключом не существует");
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine("Ошибка входных данных, {0}", ex.Message); }
+                Console.WriteLine("Нажмите любую кнопку для продолжения");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
